5a6dadfd4dfd711ae12110290bea4b1a12534974
Ivan <ivancheresky@gmail.com>
2023-02-06 18:04:49 -0300

    Reduce padding on mobile

--- src/layouts/HeaderLayout/index.tsx
+++ src/layouts/HeaderLayout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Box, useColorModeValue, useMediaQuery } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
 import Header from "@/components/Header";
 
@@ -10,8 +10,16 @@ interface HeaderLayoutProps {
 const HeaderLayout = ({ children, currentRoute }: HeaderLayoutProps) => {
   const bg = useColorModeValue("white", "brand.black");
 
+  const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
+
   return (
-    <Box bg={bg} minW="100vw" minH="100vh" py="8" px="24">
+    <Box
+      bg={bg}
+      minW="100vw"
+      minH="100vh"
+      py={isLargerThan800 ? "8" : "2"}
+      px={isLargerThan800 ? "24" : "4"}
+    >
       <Header currentRoute={currentRoute} />
       {children}
     </Box>
