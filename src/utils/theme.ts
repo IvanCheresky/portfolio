4d582de9dd66d43886835146f092b0189dd043dc
Ivan <ivancheresky@gmail.com>
2023-02-06 01:12:01 -0300

    Cleanup and chakra setup

---
+++ src/utils/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    black: "#121212",
+    orange: "#FF4D00",
+    gray: "#878383",
+  },
+};
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config, colors });
+
+export default theme;
