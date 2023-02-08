5716600970fa3ed09c391a65010d65ee0c6199b4
Ivan <ivancheresky@gmail.com>
2023-02-08 02:56:36 -0300

    Improve timeline line color on light mode

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { VerticalTimeline } from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
 import MooveItElement from "./MooveItElement";
 import LPElement from "./LPElement";
 import ZuvviiElement from "./ZuvviiElement";
 import DSElement from "./DSElement";
 import UBAElement from "./UBAElement";
+import "react-vertical-timeline-component/style.min.css";
+import { colors } from "@/utils/theme";
+import { useColorModeValue } from "@chakra-ui/react";
 
 const Timeline = () => {
+  const lineColor = useColorModeValue(colors.brand.orange, "white");
+
   return (
-    <VerticalTimeline>
+    <VerticalTimeline lineColor={lineColor}>
       <MooveItElement />
       <LPElement />
       <ZuvviiElement />
