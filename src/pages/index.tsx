82180f0fecabed64ca2e5346b5787357ffdc8d7f
Ivan <ivancheresky@gmail.com>
2023-02-08 05:04:26 -0300

    Minor improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import HeaderLayout from "@/layouts/HeaderLayout";
 import routes from "@/utils/routes";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,19 +10,25 @@ export default function Home() {
         <Text color="brand.orange" fontSize="90px" pt="15%">
           Ivan Cheresky
         </Text>
-        <Text fontSize="30px">Software Developer</Text>
-        <Button
-          bg="brand.orange"
-          textTransform="uppercase"
-          w="auto"
-          border="3px solid"
-          borderColor="brand.orange"
-          borderRadius="6px"
-          px="24px"
-          mt="3%"
-        >
-          Contact me
-        </Button>
+        <Text fontSize="30px" mb="10px">
+          Software Developer
+        </Text>
+        <Link href={routes.contact.path}>
+          <Text
+            bg="brand.orange"
+            textTransform="uppercase"
+            w="auto"
+            border="3px solid"
+            borderColor="brand.orange"
+            borderRadius="6px"
+            px="24px"
+            mt="3%"
+            py="5px"
+            fontWeight="bold"
+          >
+            Contact me
+          </Text>
+        </Link>
       </Flex>
     </HeaderLayout>
   );
