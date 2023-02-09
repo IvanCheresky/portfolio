70bdf07414ae8ed48ce65890d824a6983fc823fb
Ivan <ivancheresky@gmail.com>
2023-02-09 15:10:07 -0300

    Add meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/utils/theme";
 import { IBM_Plex_Mono } from "@next/font/google";
+import Head from "next/head";
 
 const ibm = IBM_Plex_Mono({ weight: "400", subsets: ["latin"] });
 
@@ -9,6 +10,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={ibm.className}>
       <ChakraProvider theme={theme}>
+        <Head>
+          <meta
+            name="description"
+            content="Ivan Cheresky personal portfolio and blog."
+            key="desc"
+          />
+        </Head>
         <Component {...pageProps} />
       </ChakraProvider>
     </main>
